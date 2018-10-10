504397d73cf7976067d1d004d516cd684c46a5b2
github username <slonanuga@gmail.com>
2018-10-10 17:25:01 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
-# code here!
+
+require "pry"
+class School
+
+ attr_reader :roster, :name, :grade
+
+def initialize(roster)
+   @roster = {}
+
+end
+
+
+
+def add_student(name, grade)
+    @name = name
+    @grade = grade
+
+    if roster[@grade] 
+        roster[@grade] << @name   
+    
+    else
+        roster[@grade] = []
+        roster[@grade] << @name
+    end
+end
+
+def grade(grade)
+@roster[grade]
+
+end
+
+def sort
+@roster = @roster.sort.to_h
+@roster.map {|k,v|  @roster[k] = v.sort}
+
+@roster
+end
+
+
+
+
+
+end
